8d5711d8ec6f3f5d793132910462829e0ac6982f
Your Name <you@example.com>
2026-08-25 20:10:51 +0200

    update code

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   {
-    ignores: ['dist/**', 'node_modules/**'],
+    ignores: ['dist/**', 'node_modules/**', 'commander/**'],
   },
   js.configs.recommended,
   {
