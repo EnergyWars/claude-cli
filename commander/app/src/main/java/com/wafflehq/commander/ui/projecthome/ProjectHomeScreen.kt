6cf49fd921de9ad9da02c08ec7d17c823e803ac9
Simon <mondfalke55@gmail.com>
2026-09-14 22:46:40 +0200

    update code

--- commander/app/src/main/java/com/wafflehq/commander/ui/projecthome/ProjectHomeScreen.kt
+++ commander/app/src/main/java/com/wafflehq/commander/ui/projecthome/ProjectHomeScreen.kt
@@ -12,7 +12,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ChevronRight
 import androidx.compose.material.icons.outlined.KeyboardArrowDown
@@ -86,6 +88,7 @@ fun ProjectHomeScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .statusBarsPadding()
+                .verticalScroll(rememberScrollState())
                 .padding(AppSpacing.lg),
             verticalArrangement = Arrangement.spacedBy(AppSpacing.lg),
         ) {
